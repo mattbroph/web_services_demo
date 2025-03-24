1f3d7b88fa182e7508e8e904838536a40d27b397
Matt Brophy <mattbroph@gmail.com>
2025-03-24 15:03:31 -0500

    added json and html response options for get greetings request

--- src/main/java/com/mattbroph/restdemo/HelloWorld.java
+++ src/main/java/com/mattbroph/restdemo/HelloWorld.java
@@ -41,4 +41,37 @@ import javax.ws.rs.core.Response;
             return Response.status(200).entity(output).build();
         }
 
+        /**
+         *  Responds with a dynamic message to the client in html format
+         *  Url to hit is http://localhost:8080/week_8_web_services_war/services/greetings/{name}/html
+         *
+         * @param name the name to use in the message
+         * @return a dynamic message that includes the name param in html format
+         */
+        @GET
+        @Path("{name}/html")
+        @Produces("text/html")
+        public Response getMessageWithNameParamReturnHtml(@PathParam("name") String name) {
+            // Return a dynamic message with the name that was passed in
+            String output = "<html><body><h1>Hello " + name + "!</h1></body></html>";
+            return Response.status(200).entity(output).build();
+        }
+
+        /**
+         *  Responds with a dynamic message to the client in json format
+         *  Url to hit is http://localhost:8080/week_8_web_services_war/services/greetings/{name}/json
+         *
+         * @param name the name to use in the message
+         * @return a dynamic message that includes the name param in json format
+         */
+        @GET
+        @Path("{name}/json")
+        @Produces("application/json")
+        public Response getMessageWithNameParamReturnJson(@PathParam("name") String name) {
+            // Return a dynamic message with the name that was passed in
+            String output = "{ \"message\": \"Hello " + name + "!\" }";
+            return Response.status(200).entity(output).build();
+        }
+
+
     }
